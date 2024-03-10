c9d0a04e42161c998943d927425d528bc05c3e84
xddxdd <xddxdd@users.noreply.github.com>
2024-03-10 06:13:30 +0000

    auto: update packages

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -66,7 +66,7 @@
     version = "11583";
     src = fetchurl {
       url = "https://github.com/tiann/KernelSU";
-      sha256 = "sha256-gKL/8WSWysDC1v7TQrjN0PHAewrZGf3wJw0V9uAZ8sw=";
+      sha256 = "sha256-VSaNBHTjvjGdsZuisx56ROowBOZQ1AFWOs8YjbGMung=";
     };
   };
   linux-amazon-karnak = {
