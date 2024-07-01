a6fce679d1d8cd0c5de9216adfcc53cbcda63e47
Lan Tian <xuyh0120@outlook.com>
2024-06-30 17:22:14 -0700

    general: try disable hardening flags that break kernel builds

--- pipeline/build-kernel-clang.nix
+++ pipeline/build-kernel-clang.nix
@@ -30,84 +30,85 @@
   makeFlags,
   additionalKernelConfig ? "",
   ...
-}: let
-  finalMakeFlags =
-    [
-      "ARCH=${arch}"
-      "CC=clang"
-      "O=$out"
-      "LD=ld.lld"
-      "LLVM=1"
-      "LLVM_IAS=1"
-      "CLANG_TRIPLE=aarch64-linux-gnu-"
-    ]
-    ++ makeFlags;
+}:
+let
+  finalMakeFlags = [
+    "ARCH=${arch}"
+    "CC=clang"
+    "O=$out"
+    "LD=ld.lld"
+    "LLVM=1"
+    "LLVM_IAS=1"
+    "CLANG_TRIPLE=aarch64-linux-gnu-"
+  ] ++ makeFlags;
 
   defconfig = lib.last defconfigs;
 
   usedLLVMPackages = pkgs."llvmPackages_${builtins.toString clangVersion}";
 in
-  usedLLVMPackages.stdenv.mkDerivation {
-    name = "clang-kernel-${builtins.toString clangVersion}";
-    src = src;
+usedLLVMPackages.stdenv.mkDerivation {
+  name = "clang-kernel-${builtins.toString clangVersion}";
+  src = src;
 
-    nativeBuildInputs = [
-      bc
-      bc
-      bison
-      coreutils
-      cpio
-      elfutils
-      flex
-      gmp
-      kmod
-      libmpc
-      mpfr
-      nettools
-      openssl
-      pahole
-      perl
-      python3
-      rsync
-      ubootTools
-      which
-      zlib
-      zstd
+  nativeBuildInputs = [
+    bc
+    bc
+    bison
+    coreutils
+    cpio
+    elfutils
+    flex
+    gmp
+    kmod
+    libmpc
+    mpfr
+    nettools
+    openssl
+    pahole
+    perl
+    python3
+    rsync
+    ubootTools
+    which
+    zlib
+    zstd
 
-      usedLLVMPackages.bintools
-    ];
+    usedLLVMPackages.bintools
+  ];
 
-    buildPhase =
-      ''
-        runHook preBuild
+  hardeningDisable = [ "stackprotector" ];
 
-        export CFG_PATH=arch/${arch}/configs/${defconfig}
-        cat >>$CFG_PATH <<EOF
-        ${additionalKernelConfig}
-        EOF
-      ''
-      + (lib.optionalString enableKernelSU ''
-        # Inject KernelSU options
-        echo "CONFIG_MODULES=y" >> $CFG_PATH
-        echo "CONFIG_KPROBES=y" >> $CFG_PATH
-        echo "CONFIG_HAVE_KPROBES=y" >> $CFG_PATH
-        echo "CONFIG_KPROBE_EVENTS=y" >> $CFG_PATH
-        echo "CONFIG_OVERLAY_FS=y" >> $CFG_PATH
-      '')
-      + ''
-        mkdir -p $out
-        make ${builtins.concatStringsSep " " (finalMakeFlags ++ defconfigs)}
+  buildPhase =
+    ''
+      runHook preBuild
 
-        runHook postBuild
-      '';
+      export CFG_PATH=arch/${arch}/configs/${defconfig}
+      cat >>$CFG_PATH <<EOF
+      ${additionalKernelConfig}
+      EOF
+    ''
+    + (lib.optionalString enableKernelSU ''
+      # Inject KernelSU options
+      echo "CONFIG_MODULES=y" >> $CFG_PATH
+      echo "CONFIG_KPROBES=y" >> $CFG_PATH
+      echo "CONFIG_HAVE_KPROBES=y" >> $CFG_PATH
+      echo "CONFIG_KPROBE_EVENTS=y" >> $CFG_PATH
+      echo "CONFIG_OVERLAY_FS=y" >> $CFG_PATH
+    '')
+    + ''
+      mkdir -p $out
+      make ${builtins.concatStringsSep " " (finalMakeFlags ++ defconfigs)}
 
-    installPhase = ''
-      runHook preInstall
-
-      make -j$(nproc) ${builtins.concatStringsSep " " finalMakeFlags}
-
-      runHook postInstall
+      runHook postBuild
     '';
 
-    dontFixup = true;
-  }
+  installPhase = ''
+    runHook preInstall
+
+    make -j$(nproc) ${builtins.concatStringsSep " " finalMakeFlags}
+
+    runHook postInstall
+  '';
+
+  dontFixup = true;
+}
