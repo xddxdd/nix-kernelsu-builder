2235977d5422054f73354617b0409590875f46e2
Lan Tian <xuyh0120@outlook.com>
2024-07-02 00:25:48 -0700

    treewide: format

--- flake-modules/commands.nix
+++ flake-modules/commands.nix
@@ -1,14 +1,16 @@
 _: {
-  perSystem = { pkgs, inputs', ... }: {
-    commands = {
-      nvfetcher = ''
-        set -euo pipefail
-        KEY_FLAG=""
-        [ -f "$HOME/Secrets/nvfetcher.toml" ] && KEY_FLAG="$KEY_FLAG -k $HOME/Secrets/nvfetcher.toml"
-        [ -f "secrets.toml" ] && KEY_FLAG="$KEY_FLAG -k secrets.toml"
-        export PATH=${pkgs.nix-prefetch-scripts}/bin:$PATH
-        ${inputs'.nvfetcher.packages.default}/bin/nvfetcher $KEY_FLAG -c nvfetcher.toml -o _sources "$@"
-      '';
+  perSystem =
+    { pkgs, inputs', ... }:
+    {
+      commands = {
+        nvfetcher = ''
+          set -euo pipefail
+          KEY_FLAG=""
+          [ -f "$HOME/Secrets/nvfetcher.toml" ] && KEY_FLAG="$KEY_FLAG -k $HOME/Secrets/nvfetcher.toml"
+          [ -f "secrets.toml" ] && KEY_FLAG="$KEY_FLAG -k secrets.toml"
+          export PATH=${pkgs.nix-prefetch-scripts}/bin:$PATH
+          ${inputs'.nvfetcher.packages.default}/bin/nvfetcher $KEY_FLAG -c nvfetcher.toml -o _sources "$@"
+        '';
+      };
     };
-  };
 }
