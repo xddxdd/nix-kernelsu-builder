3ba6282f709375dd94ffa16bd8d74be7e92449dd
Lan Tian <xuyh0120@outlook.com>
2025-02-09 22:31:46 +0800

    gcc: download directly from google

--- pkgs/gcc-arm-linux-androideabi.nix
+++ pkgs/gcc-arm-linux-androideabi.nix
@@ -1,15 +1,18 @@
 {
   stdenv,
   lib,
-  callPackage,
+  fetchurl,
   autoPatchelfHook,
   python3,
 }:
-let
-  sources = callPackage ../_sources/generated.nix { };
-in
-stdenv.mkDerivation {
-  inherit (sources.gcc-arm-linux-androideabi) pname version src;
+stdenv.mkDerivation rec {
+  pname = "gcc-arm-linux-androideabi";
+  version = "12.1.0_r27";
+  src = fetchurl {
+    url = "https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/+archive/refs/tags/android-${version}.tar.gz";
+    sha256 = "sha256-jOjJ10kLpdB1i3zJ5JWoQnjq4oWsR1rpayusmzfQTc0=";
+  };
+  sourceRoot = ".";
 
   nativeBuildInputs = [ autoPatchelfHook ];
   buildInputs = [ python3 ];
