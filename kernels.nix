d4f7cb7605e652eb86d7e7a28b5385a570ee3c78
Lan Tian <xuyh0120@outlook.com>
2025-07-22 20:10:59 -0700

    kenrel: try to fix oneplus 13 bootloop again (and failed)

--- kernels.nix
+++ kernels.nix
@@ -92,7 +92,7 @@ _: {
         # DOESN'T BOOT FOR NOW
         oneplus-13 = {
           anyKernelVariant = "kernelsu";
-          clangVersion = "latest";
+          clangVersion = "18";
           kernelSU.variant = "sukisu-susfs";
           susfs = {
             enable = true;
@@ -110,12 +110,41 @@ _: {
             };
           };
           # https://github.com/HanKuCha/oneplus13_a5p_sukisu/blob/main/.github/workflows/oneplus_13.yml
+          # https://github.com/aisessss/build_oneplus_sm8750/blob/main/Build_sm8750.sh
           kernelConfig = ''
-            CONFIG_CRYPTO_842=y
-            CONFIG_CRYPTO_LZ4HC=y
-            CONFIG_CRYPTO_LZ4K=y
-            CONFIG_CRYPTO_LZ4KD=y
+            CONFIG_KSU=y
+            CONFIG_KSU_SUSFS_SUS_SU=n
+            CONFIG_KSU_MANUAL_HOOK=y
+            CONFIG_KSU_SUSFS=y
+            CONFIG_KSU_SUSFS_HAS_MAGIC_MOUNT=y
+            CONFIG_KSU_SUSFS_SUS_PATH=y
+            CONFIG_KSU_SUSFS_SUS_MOUNT=y
+            CONFIG_KSU_SUSFS_AUTO_ADD_SUS_KSU_DEFAULT_MOUNT=y
+            CONFIG_KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT=y
+            CONFIG_KSU_SUSFS_SUS_KSTAT=y
+            CONFIG_KSU_SUSFS_TRY_UMOUNT=y
+            CONFIG_KSU_SUSFS_AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT=y
+            CONFIG_KSU_SUSFS_SPOOF_UNAME=y
+            CONFIG_KSU_SUSFS_ENABLE_LOG=y
+            CONFIG_KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS=y
+            CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG=y
+            CONFIG_KSU_SUSFS_OPEN_REDIRECT=y
 
+            # 启用高级压缩支持
+            CONFIG_CRYPTO_LZ4HC=y
+            CONFIG_CRYPTO_LZ4=y
+            CONFIG_CRYPTO_ZSTD=y
+
+            # 文件系统级压缩支持
+            CONFIG_F2FS_FS_COMPRESSION=y
+            CONFIG_F2FS_FS_LZ4=y
+            CONFIG_F2FS_FS_LZ4HC=y
+            CONFIG_F2FS_FS_ZSTD=y
+
+            # 内核镜像压缩配置
+            CONFIG_KERNEL_LZ4=y
+
+            # BBR(TCP拥塞控制算法)
             CONFIG_TCP_CONG_ADVANCED=y
             CONFIG_TCP_CONG_BBR=y
             CONFIG_NET_SCH_FQ=y
@@ -124,6 +153,8 @@ _: {
             CONFIG_TCP_CONG_WESTWOOD=n
             CONFIG_TCP_CONG_HTCP=n
             CONFIG_DEFAULT_TCP_CONG=bbr
+
+            CONFIG_LOCALVERSION_AUTO=n
           '';
           kernelDefconfigs = [ "gki_defconfig" ];
           kernelImageName = "Image";
@@ -141,15 +172,15 @@ _: {
             install -Dm644 ${./resources/oneplus-13-hmbird-patch.c} drivers/hmbird_patch.c
             echo "obj-y += hmbird_patch.o" >> drivers/Makefile
 
-            # https://github.com/HanKuCha/oneplus13_a5p_sukisu/blob/1604ce2607a04c4d9b8f1ba426a601bedac6d989/.github/workflows/oneplus_13.yml#L105-L109
-            cp -r ${sources.sukisu-patch.src}/other/zram/lz4k/include/linux/* include/linux/
-            cp -r ${sources.sukisu-patch.src}/other/zram/lz4k/lib/* lib/
-            cp -r ${sources.sukisu-patch.src}/other/zram/lz4k/crypto/* crypto/
-            cp -r ${sources.sukisu-patch.src}/other/zram/lz4k_oplus lib/
-            patch -p1 -F3 < ${sources.sukisu-patch.src}/other/zram/zram_patch/6.6/lz4kd.patch
-
             # https://github.com/HanKuCha/oneplus13_a5p_sukisu/blob/1604ce2607a04c4d9b8f1ba426a601bedac6d989/.github/workflows/oneplus_13.yml#L264-L271
             cp -r ${sources.oneplus-13-sched-ext.src}/* kernel/sched/
+
+            substituteInPlace build.config.gki \
+              --replace-fail "check_defconfig" ""
+            substituteInPlace scripts/setlocalversion \
+              --replace-fail '${"\${scm_version}"}' ""
+            substituteInPlace arch/arm64/configs/gki_defconfig \
+              --replace-fail "-4k" "-android15-8-g013ec21bba94-abogki383916444-4k"
 
             chmod -R +w .
           '';
